edf2e4cc533f7dc5f88d24530c467cced3a1dc32
Jeff Fowler <jeffowler@gmail.com>
2018-02-20 21:52:02 +0100

    fj

--- src/builtins/loaders/browser/wasm-loader.js
+++ src/builtins/loaders/browser/wasm-loader.js
@@ -2,11 +2,16 @@ module.exports = function loadWASMBundle(bundle) {
   return fetch(bundle)
     .then(function (res) {
       if (WebAssembly.instantiateStreaming) {
-        return WebAssembly.instantiateStreaming(res);
+        return WebAssembly.instantiateStreaming(res, {
+          env: {
+            __linear_memory: new WebAssembly.Memory({initial: 10}),
+            __indirect_function_table: new WebAssembly.Table({element:'anyfunc', initial: 10})
+          }
+        });
       } else {
         return res.arrayBuffer()
           .then(function (data) {
-            return WebAssembly.instantiate(data);
+            return WebAssembly.instantiate(data, {});
           });
       }
     })
